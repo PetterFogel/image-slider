1730912daf3a0c7879762247f61819a193d0d924
unknown <petter.fogel@medieinstitutet.se>
2020-12-07 20:08:37 +0100

    Add new logic for image slider.

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 window.addEventListener("load", main);
 
 function main() {
-    setIntervalToReloadPage();
+    startProgramWithSetinterval();
 }
 
-function setIntervalToReloadPage() {
-    setInterval(reloadPage, 1000);
+function startProgramWithSetinterval() {
+    setInterval(fetchImagesFromHTML, 4000);
 }
 
-function reloadPage() {
-    location.reload();
+function fetchImagesFromHTML() {
+    const images = document.querySelectorAll(".slider-container > img");
+    changeActiveImage(images);
+}
+
+function changeActiveImage(images) {
+    for (let i = 0; i < images.length; i++) {
+
+        if (images[i].classList.contains("active")) {
+            images[i].classList.remove("active");
+            let nextIndex = i + 1;
+
+            if (nextIndex >= images.length) {
+                nextIndex = 0;
+            }
+
+            images[nextIndex].classList.add("active");
+            break;
+        }
+    }
 }
